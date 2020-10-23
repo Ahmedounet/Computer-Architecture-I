dc5a28e063b16439cc02d6c04b7aeb770fe4bf77
Victor B. Gergaud <61604940+vgergaud@users.noreply.github.com>
2020-10-23 11:05:40 +0200

    Update controller.vhd

--- template_lab_3/vhdl/controller.vhd
+++ template_lab_3/vhdl/controller.vhd
@@ -283,21 +283,20 @@ begin
                 sel_ra <= '1';
                 pc_en <= '1';
                 rf_wren <= '1';
-                case opx is 
-                    when "011101" =>
-                        pc_sel_a <= '1';
-                    when others =>
-                        pc_sel_imm <= '1';
-                end case;
+                if opx = "011101" then
+                    pc_sel_a <= '1';
+                else
+                    pc_sel_imm <= '1';
+                end if;
                 s_next_state <= FETCH1;
             when JMP =>
                 -- Common signal for any JMP 
                 pc_en <= '1';
                 case opx is 
-                    when "000001" =>
-                        pc_sel_imm <= '1';
-                    when others =>
+                    when "000101" | "001101"=>
                         pc_sel_a <= '1';
+                    when others =>
+                        pc_sel_imm <= '1';
                 end case;
                 s_next_state <= FETCH1;
             ------------------------- END Part 4 ---------------------
